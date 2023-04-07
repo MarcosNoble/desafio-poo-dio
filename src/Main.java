c7f101c7dbe5fe3e15107361d1a50a622aaa90fb
Marcos <marcos.luz.noble@gmail.com>
2023-04-07 17:57:37 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -21,9 +19,43 @@ public class Main {
         mentoria.setTitulo("mentoria de Java");;
         mentoria.setDescricao("descrição de mentoria em Jaba");
         mentoria.setData(LocalDate.now());
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp teste java");
+        bootcamp.setDescricao("descrição descritiva");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
+        Dev devTeste1 = new Dev();
+        devTeste1.setNome("senhor1");
+        devTeste1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Sr1" + devTeste1.getConteudosInscritos());
+        devTeste1.progredir();
+        System.out.println("progressão");
+        System.out.println("Conteudos concluidos Sr1" + devTeste1.getConteudosConcluidos());
+        System.out.println("XP Sr1: " + devTeste1.calcularTotalXp());
+
+        System.out.println("____________");
+
+        Dev devTeste2 = new Dev();
+        devTeste2.setNome("senhor2");
+        devTeste2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Sr2" + devTeste2.getConteudosInscritos());
+        devTeste2.progredir();
+        System.out.println("progressão");
+        devTeste2.progredir();
+        System.out.println("progressão");
+
+        System.out.println("Conteudos concluidos Sr2" + devTeste2.getConteudosConcluidos());
+        System.out.println("XP Sr2: " + devTeste2.calcularTotalXp());
+        System.out.println("____________");
+        System.out.println("Após progressão podemos ver a diferença");
+        System.out.println("Conteudos inscritos Sr2" + devTeste2.getConteudosInscritos());
+
+
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
     }
 }
