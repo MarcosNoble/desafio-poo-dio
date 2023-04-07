997eef7584f9875621a6e42fb5345d5fd1dfe70a
Marcos <marcos.luz.noble@gmail.com>
2023-04-07 15:55:34 -0300

    Classe abstrata Conteudo e herança nas outras classes.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -14,6 +15,7 @@ public class Main {
         curso2.setTitulo("curso js");
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de Java");;
